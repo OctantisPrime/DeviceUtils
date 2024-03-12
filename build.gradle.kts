5942844d93efe55fa7f5a9047ff56d568703e15e
Csx <csx_mail@163.com>
2024-03-12 16:52:43 +0800

    rebuild

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.android.library") version "8.2.2" apply false
+    id("com.android.library") version "7.4.2" apply false
 }
 
 buildscript {
+    val agp_version by extra("7.4.2")
     repositories {
         google()
         mavenCentral()
